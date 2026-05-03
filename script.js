9910cf6d5a741113f16d324e0fc7eeff006ddc30
Tanush18-coder <YTindia74@gmail.com>
2026-05-03 19:50:05 +0530

    Third Commit

--- script.js
+++ script.js
@@ -1,27 +1,16 @@
 let slideIndex = 0;
-
 const slides = document.querySelectorAll(".slide");
-
 function showSlides(n) {
-
     if (n >= slides.length) {
-
         slideIndex = 0;
 
     } else if (n < 0) {
-
         slideIndex = slides.length - 1;
-
     } else {
-
         slideIndex = n;
-
     }
-
     slides.forEach(slide => {
-
         slide.classList.remove("active");
-
     });
 
     slides[slideIndex].classList.add("active");
@@ -31,7 +20,29 @@ function changeSlide(n) {
     showSlides(slideIndex + n);
 }
 setInterval(() => {
-
     changeSlide(1);
-
 }, 1000);
+
+let currentMenuIndex = 0;
+const menuPages = document.querySelectorAll(".menu-page");
+
+function showMenuPage(n) {
+    if (menuPages.length === 0)
+
+        if (n >= 0) {
+currentMenuIndex = 0;
+        }
+    else if (n < 0) {
+        currentMenuIndex = menuPages.length - 1;
+    }
+    else {
+        currentMenuIndex = n
+    }
+    menuPages.forEach(page => {
+        page.classList.remove("active");
+    })
+}
+
+function changeMenuPage(n) {
+    showMenuIndex(currentMenuIndex + n);
+}
